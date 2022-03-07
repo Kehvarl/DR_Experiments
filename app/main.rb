851cd43e9fa1fb43b6552e03e97d80d8e789fd10
Kehvarl <Kehvarl@Kehvarl.com>
2022-03-07 07:30:42 -0700

    smooth scolling render_target

--- app/main.rb
+++ app/main.rb
@@ -48,11 +48,28 @@ class Ground_Generate
     while x + w < width
       arr << {x:x, y:y, x2:x+w, y2:target_y, r:128, g:128, b:128}.line!
       x += w
-      w = [rand(100), (width - x - 25)].min
+      w = [rand(100), (width - x)].min
       y = target_y
       target_y += rand(max_change + max_change) - max_change
-      if width - x <= 50
+      if target_y < min_h
+        target_y = min_h
+      end
+      if target_y > max_h
+        target_y = max_h
+      end
+      if x <= 1280
+        arr << {x:x+2560, y:y, x2:x+2560+w, y2:target_y, r:128, g:128, b:128}.line!
+        if 1280 - x <= 100 and x < 1280
+          arr << {x:x, y:y, x2:1280, y2:min_h, r:128, g:128, b:128}.line!
+          arr << {x:x+2560, y:y, x2:3840, y2:min_h, r:128, g:128, b:128}.line!
+          x = 1280
+          y = min_h
+          target_y = min_h
+        end
+      end
+      if width - x <= 100
         arr << {x:x, y:y, x2:width, y2:min_h, r:128, g:128, b:128}.line!
+        #arr << {x:x+2560, y:y, x2:5120, y2:min_h, r:128, g:128, b:128}.line!
         break
       end
     end
@@ -60,8 +77,8 @@ class Ground_Generate
   end
 
   def ground_render args
-    args.outputs[:ground].w = 2560
-    args.outputs[:ground].h = 720
+    args.outputs[:ground].w = 5120
+    args.outputs[:ground].h = 180
     args.outputs[:ground].primitives <<{x:0, y:0, w:1280, h:720, r:0, g:0, b:0}.solid!
     args.outputs[:ground].primitives << @ground.map do |g|
       g
@@ -75,17 +92,22 @@ class Ground_Generate
     if args.inputs.keyboard.key_down.left
       @vx -= 1
     end
-    @x += @vx
+    @x = (@x + @vx) % 2560
     args.outputs.primitives <<{x:0, y:0, w:1280, h:720, r:0, g:0, b:0}.solid!
     #args.outputs.primitives << @ground.map do |g|
     #  g[:x] = (g[:x] - @vx) % 2560
     #  g[:x2] = (g[:x2] - @vx) % 2560
     #  g
     #end
-    args.outputs.primitives << {x: 0, y: 0, w: 1280, h: 720,
+    args.outputs.primitives << {x: 320, y: 629, w: 640, h: 90,
+                                path: :ground,
+                                source_x: 0, source_y: 0,
+                                source_w: 2560, source_h: 180}.sprite!
+    args.outputs.primitives <<{x:319, y:628, w:641, h:91, r:0, g:128, b:0}.border!
+    args.outputs.primitives << {x: 0, y: 0, w: 1280, h: 180,
                                 path: :ground,
                                 source_x: @x, source_y: 0,
-                                source_w: 1280, source_h: 720}
+                                source_w: 1280, source_h: 180}.sprite!
   end
 end
 
