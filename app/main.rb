28cdee43b1985138de4fe5d200784578451dd2d1
Kehvarl <Kehvarl@Kehvarl.com>
2022-03-13 17:50:26 -0700

    playing with a better minimap

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,6 @@
 class Ground_Generate
   def initialize args
-    @ground = generate_ground
+    @ground = generate_ground_lines
     ground_render args
     generate_ship args
     @last = 'generate_ground'
@@ -43,7 +43,14 @@ class Ground_Generate
     args.outputs[:ship].primitives << {x: x, y: 32, x2: x+16, y2: 16, r: 128, g: 128, b: 128}.line!
     args.outputs[:ship].primitives << {x: x+8, y:  8, x2: x, y2: 16, r: 128, g: 128, b: 128}.line!
     args.outputs[:ship].primitives << {x: x+8, y: 24, x2: x, y2: 16, r: 128, g: 128, b: 128}.line!
-
+    args.outputs[:mini_ship].w = 12
+    args.outputs[:mini_ship].h = 8
+    args.outputs[:mini_ship].background_color = [255, 255, 255, 0]
+    x = 0
+    args.outputs[:mini_ship].primitives << {x: x, y: 0, x2: x+12, y2: 4, r: 128, g: 128, b: 128}.line!
+    args.outputs[:mini_ship].primitives << {x: x, y: 8, x2: x+12, y2: 4, r: 128, g: 128, b: 128}.line!
+    args.outputs[:mini_ship].primitives << {x: x, y: 0, x2: x+ 4, y2: 4, r: 128, g: 128, b: 128}.line!
+    args.outputs[:mini_ship].primitives << {x: x, y: 8, x2: x+ 4, y2: 4, r: 128, g: 128, b: 128}.line!
   end
 
   def generate_ground(width=2560, max_h=180, min_h = 5, max_change=5)
@@ -74,9 +81,9 @@ class Ground_Generate
         h -= rand(max_change)
       end
       arr << {x:x, y:0, w:w, h:h, r:64, g:64, b:64}.solid!
-      arr << {x:x, y:h, w:w, h:1, r:0, g:255, b:0}.solid!
+      arr << {x:x, y:h, w:w+3, h:4, r:0, g:255, b:0}.solid!
       arr << {x:x+2560, y:0, w:w, h:h, r:64, g:64, b:64}.solid!
-      arr << {x:x+2560, y:h, w:w, h:1, r:0, g:255, b:0}.solid!
+      arr << {x:x+2560, y:h, w:w+3, h:4, r:0, g:255, b:0}.solid!
     end
     arr
   end
@@ -110,6 +117,7 @@ class Ground_Generate
         end
       end
       if width - x <= 100
+        arr << {x:x, y:y, x2:width, y2:min_h, r:128, g:128, b:128}.line!
         arr << {x:x, y:y, x2:width, y2:min_h, r:128, g:128, b:128}.line!
         break
       end
@@ -150,7 +158,7 @@ class Ground_Generate
     end
     if args.inputs.keyboard.key_down.space
       if @ship_flipped
-        x = 640
+        x = 576
         v = -16
       else
         x = 672
@@ -169,17 +177,8 @@ class Ground_Generate
                                         path: :ground,
                                         source_x: @x, source_y: 0,
                                         source_w: 1280, source_h: 720}.sprite!
-    args.outputs[:minimap].primitives << {x: 0, y: 0, w: 2560, h: 720,
-                                          path: :ground,
-                                          source_x: 0, source_y: 0,
-                                          source_w: 2560, source_h: 720}.sprite!
     # Ship
     args.outputs[:scene].primitives << {x: 608, y: @y, w: 64, h: 32,
-                                        flip_horizontally: @ship_flipped,
-                                        path: :ship,
-                                        source_x: @frame * 64, source_y: 0,
-                                        source_w: 64, source_h: 32}.sprite!
-    args.outputs[:minimap].primitives << {x: (@x + 608)%2560, y: @y, w: 64, h: 32,
                                         flip_horizontally: @ship_flipped,
                                         path: :ship,
                                         source_x: @frame * 64, source_y: 0,
@@ -189,10 +188,30 @@ class Ground_Generate
                                 path: :scene,
                                 source_x: 0, source_y: 0,
                                 source_w: 1280, source_h: 720}.sprite!
-    args.outputs.primitives << {x: 380, y: 629, w: 480, h: 90,
-                                path: :minimap}.sprite!
 
-    args.outputs.primitives <<{x:379, y:628, w:481, h:91, r:0, g:128, b:0}.border!
+    args.outputs.primitives << {x: (@x/6 + 510)%2560, y: @y/4 + 539, w: 12, h: 8,
+                                          flip_horizontally: @ship_flipped,
+                                          path: :mini_ship}.sprite!
+
+    args.outputs.primitives << @ground.map do |g|
+      t = g.copy
+      if t.x <= 2560
+        t.x = t.x/4 + 320
+        t.y = t.y/4 + 539
+        if t.x2
+          t.x2 = t.x2/4 + 320
+          t.y2 = t.y2/4 + 539
+        else
+          t.w = t.w/4
+          t.h = t.h/4
+        end
+        t
+      end
+    end
+
+    args.outputs.primitives <<{x:319, y:538, w:641, h:181, r:0, g:128, b:0}.border!
+
+
 
     @projectiles.each do |p|
       p[0] += p[3]
