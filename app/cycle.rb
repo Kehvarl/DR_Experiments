3f190d0df949e85088a20ebca6c5ef17c2152200
Kehvarl <Kehvarl@Kehvarl.com>
2022-04-11 21:02:30 -0700

    working on playfield

--- app/cycle.rb
+++ app/cycle.rb
@@ -6,6 +6,11 @@ class Cycle
     @y = 480
     @vx = 0
     @vy = 0
+    @tiles = Array.new(120){Array.new(160, 0)}
+    @tiles[rand(120)][rand(160)] = rand(128)+128
+    @tiles[rand(120)][rand(160)] = rand(128)+128
+    @tiles[rand(120)][rand(160)] = rand(128)+128
+    @tiles[rand(120)][rand(160)] = rand(128)+128
   end
 
   def render
@@ -36,5 +41,10 @@ class Cycle
     @x += @vx
     @y += @vy
     args.outputs.solids << {x: @x, y: @y, w: 24, h: 32, r:128, g:0, b:128}
+    for y in 0..119
+      for x in 0..159
+        args.outputs.solids << {x: x*8, y: y*8, w: 8, h: 8, r:@tiles[y][x], g:0, b:0}
+      end
+    end
   end
 end
